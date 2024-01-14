60cc0e250a3a519b3dba66cbda986ca06510539d
Maharshi85 <maharshi.pradhan@gmail.com>
2024-01-14 11:07:08 +0530

    first commit

---
+++ PracticeSelenium/src/normalproject/in/Practice_One.java
@@ -0,0 +1,22 @@
+package normalproject.in;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Practice_One {
+
+	public static void main(String[] args) {
+		//WebDriver dri=new ChromeDriver();
+		WebDriver dri=new EdgeDriver();
+		dri.manage().window().maximize();
+		dri.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		dri.get("http://localhost:8888");
+		dri.findElement(By.name("user_name")).sendKeys("admin");
+		dri.findElement(By.name("user_password")).sendKeys("password");
+		dri.findElement(By.id("submitButton")).click();
+	}
+}
